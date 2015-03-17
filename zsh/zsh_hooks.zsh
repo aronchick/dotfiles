a468f7f46ce10cea0106675df8280a47f8454cac
David Aronchick <aronchick@google.com>
2015-03-17 10:54:26 -0700

    removing set_running_app hooks because it does not work on ubuntu

--- zsh/zsh_hooks.zsh
+++ zsh/zsh_hooks.zsh
@@ -8,13 +8,13 @@ function precmd {
 }
 
 function set_running_app {
-  printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
+  #printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
 }
 
 function preexec {
-  set_running_app
+  #set_running_app
 }
 
 function postexec {
-  set_running_app
+  #set_running_app
 }
